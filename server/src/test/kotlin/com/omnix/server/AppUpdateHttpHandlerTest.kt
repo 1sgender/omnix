fa5daf99201caf072d07280ae7294269dc9ea945
freelanceTM <freelanceTM@users.noreply.github.com>
2026-09-17 11:20:23 +0000

    fix(repo): update references after rename freelanceTM/jarvis -> freelanceTM/omnix
    - deploy/vps-setup.sh: git clone URL
    - AppUpdateHttpHandler.DEFAULT_REPO (OTA update source)
    - AppUpdateHttpHandlerTest fixtures

--- server/src/test/kotlin/com/omnix/server/AppUpdateHttpHandlerTest.kt
+++ server/src/test/kotlin/com/omnix/server/AppUpdateHttpHandlerTest.kt
@@ -30,7 +30,7 @@ class AppUpdateHttpHandlerTest {
     private fun releaseJson(
         versionCodeLine: String = "versionCode=35071367107",
         assetName: String = "OMNIX-staging.apk",
-        assetUrl: String = "https://github.com/freelanceTM/jarvis/releases/download/staging/OMNIX-staging.apk"
+        assetUrl: String = "https://github.com/freelanceTM/omnix/releases/download/staging/OMNIX-staging.apk"
     ) = """
         {"tag_name":"staging",
          "body":"$versionCodeLine\nsha=9782910cd3e9570d5931cd4a5f7567735c7380a3",
@@ -41,7 +41,7 @@ class AppUpdateHttpHandlerTest {
     @Test
     fun `latest returns version and download url without auth`() = runBlocking {
         val handler = AppUpdateHttpHandler(
-            releaseRepo = "freelanceTM/jarvis",
+            releaseRepo = "freelanceTM/omnix",
             fetchReleaseJson = { releaseJson() }
         )
 
